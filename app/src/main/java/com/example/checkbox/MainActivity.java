f39aceed9e37ba48621ef8e39798ff9db4d15e7d
usama <bitf18a534@pucit.edu.pk>
2021-12-31 15:28:06 +0500

    variables declare

--- app/src/main/java/com/example/checkbox/MainActivity.java
+++ app/src/main/java/com/example/checkbox/MainActivity.java
@@ -3,8 +3,18 @@ package com.example.checkbox;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    public CheckBox first,second;
+    public Button button;
+    public TextView text;
+    public ArrayList<String> result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
