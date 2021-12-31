e1d0f74aadf92a429711374692aa8af84853a540
usama <bitf18a534@pucit.edu.pk>
2021-12-31 15:39:54 +0500

    ore work

--- app/src/main/java/com/example/checkbox/MainActivity.java
+++ app/src/main/java/com/example/checkbox/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.checkbox;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
@@ -20,5 +21,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        first=findViewById(R.id.checkBox);
+        second=findViewById(R.id.checkBox2);
+        button=findViewById(R.id.button);
+        result=new ArrayList<String>();
+
+        first.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(first.isChecked())
+                {
+                    result.add("first check Box");
+                }
+            }
+        });
     }
 }
