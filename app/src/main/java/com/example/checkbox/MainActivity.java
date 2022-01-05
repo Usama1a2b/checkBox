35f9da5551babdd74ff9623ac57d00be33e5b26e
usama <bitf18a534@pucit.edu.pk>
2022-01-05 15:01:42 +0500

    some error

--- app/src/main/java/com/example/checkbox/MainActivity.java
+++ app/src/main/java/com/example/checkbox/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
         second=findViewById(R.id.checkBox2);
         button=findViewById(R.id.button);
         result=new ArrayList<String>();
+        //text.setEnabled(false);
 
         first.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,6 +35,42 @@ public class MainActivity extends AppCompatActivity {
                 {
                     result.add("first check Box");
                 }
+                else
+                {
+                    result.remove("first check Box");
+                }
+            }
+
+
+        });
+
+        second.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(second.isChecked())
+                {
+                    result.add("second check Box");
+                }
+                else
+                {
+                    result.remove("second check Box");
+                }
+
+
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                StringBuilder sb = new StringBuilder();
+                for(String a:result)
+                {
+                    sb.append(a).append("/n");
+                }
+                text.setText(sb.toString());
+                //text.setEnabled(false);
             }
         });
     }
